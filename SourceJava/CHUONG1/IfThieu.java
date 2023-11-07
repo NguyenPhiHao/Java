c5e360f7df5e3a8dd27f0a048b0413f25f7b74c8
BV_hao.nguyen-phi <hao.nguyen-phi@banvien.com.vn>
2023-11-07 11:37:07 +0700

    ok

---
+++ SourceJava/CHUONG1/IfThieu.java
@@ -0,0 +1,8 @@
+public class IfThieu {
+    public static void main(String[] args){
+        int Tuoi = 20;
+        if(Tuoi >= 18) {
+            System.out.println("Ban da du tuoi vi thanh vien");
+        }
+    }
+}
